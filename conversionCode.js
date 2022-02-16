bd6005def7e9434fade79b563b078d06039843ac
Julio Alencar <71025603+juliocsalencar@users.noreply.github.com>
2022-02-16 09:50:10 -0300

    Add files via upload

---
+++ conversionCode.js
@@ -0,0 +1,21 @@
+<script>
+document.addEventListener('DOMContentLoaded', function(){
+console.log('DOM is ready for action');
+   //conversão form 
+    if(window.location.href.includes('/contato')){
+    console.log('contactPageHere');
+        document.querySelector('form').addEventListener('submit', ()=>{
+        console.log('formSent');
+            gtag('event', 'conversion', {'send_to':'AW-875285673/CWPcCNHvrpQDEKmZr6ED'});
+        });
+    }
+});
+</script>
+
+<script>
+  var enhanced_conversion_data = {
+    "email": #V2ViRm9ybUNhcHR1cmVCbG9jazozYjc3YWRiMi1mMzAwLTExZWEtOWE1Zi0xYmZmYWFhOWVjNDg,
+    "name": #V2ViRm9ybUNhcHR1cmVCbG9jazozYjc3YWRiMC1mMzAwLTExZWEtOWE1Zi0xYmZmYWFhOWVjNDg,
+    "numberContact": #V2ViRm9ybUNhcHR1cmVCbG9jazozYjc3ZDRjMC1mMzAwLTExZWEtOWE1Zi0xYmZmYWFhOWVjNDg,
+    };
+</script>
